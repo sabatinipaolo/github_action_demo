6be51ee4b86629f4071e945c82ef6cf5243134b6
sabatinipaolo <113015247+sabatinipaolo@users.noreply.github.com>
2026-03-10 20:40:14 +0100

    Add index.php to display database connection details

---
+++ sito/index.php
@@ -0,0 +1,9 @@
+<?php 
+require ( "connessione.php" );
+
+echo "<h1>HOST =".$HOST."</h1>\n";
+echo "<h1>USER=".$USER."</h1>\n";
+echo "<h1>PASSWORD=".$PASSWORD."</h1>\n";
+echo "<h1>DBNAME=".$DBNAME."</h1>\n";
+
+?>
